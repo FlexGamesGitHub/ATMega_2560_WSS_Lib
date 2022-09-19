96c9ef0c01ad0bbea0d788a8f53289268e646a4c
FileX <79639219+cd-FileX@users.noreply.github.com>
2022-09-19 17:29:10 +0200

    Moved and updated for 0.0.3

--- Presets/arduino_preset.c
+++ Presets/arduino_preset.c
@@ -1,12 +1,10 @@
 #include "FileX_2560_Lib-0.0.2a.h"
 
 void init(void) { // Runs once
-    DDRB = 0xFF;
-    DDRA = 0xFF;
-    DDRC = 0xFF;
+    std_init({});
 }
 
 int main(void) { // Runs infinite
     
-    RT;
+    return 1;
 }
