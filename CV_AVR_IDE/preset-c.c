3e477247a952a495a26332c753ddcaf61754d288
FileX <79639219+cd-FileX@users.noreply.github.com>
2022-09-21 20:14:03 +0200

    Update preset-c.c

--- CV_AVR_IDE/preset-c.c
+++ CV_AVR_IDE/preset-c.c
@@ -1,11 +1,11 @@
 #include <mega2560.h>
 #include "FileX_2560_Lib-0.0.2a.h"
 
-void init(void) { // Runs once
+void init(void) {
     std_init({});
 }
 
-void main(void) { // Runs infinite
+void main(void) {
     init();
     
     while (1) {
